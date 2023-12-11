511b79f0469a275fd1d33a9cc6de7aea843deee9
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-12-11 12:14:06 -0300

    Update Col

--- themes/ragnazen/main/navbar.php
+++ themes/ragnazen/main/navbar.php
@@ -3,7 +3,12 @@
 <div class="online-bar">
 	<div class="container">
 		<div class="row">
-			<p>Usuários: <span class="green">522</span> Server <span><i class="fa-solid fa-toggle-on"></i></span></p>
+			<div class="col">
+			<p>Usuários: <span class="green">522</span></p>
+			</div>
+			<div class="col">
+			<p>Server <span><i class="fa-solid fa-toggle-on"></i></span></p>
+			</div>
 		</div>
 	</div>
 </div>
