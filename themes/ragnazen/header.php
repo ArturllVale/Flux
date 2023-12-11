b91b3c7a827d9e25d85ae44e0a253511e223583a
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-12-11 16:18:12 -0300

    Update Wave 4

--- themes/ragnazen/header.php
+++ themes/ragnazen/header.php
@@ -30,6 +30,7 @@
 	<div class="wave-container"></div>
 	<div class="wave-container2"></div>
 	<div class="wave-container3"></div>
+	<div class="wave-container4"></div>
     <div class="container information">
 		<?php //include 'main/sidebar.php' ?>
 		<?php include 'main/loginbox.php' ?>
