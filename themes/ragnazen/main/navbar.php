a554d2067ed72a019c4b3787a4fd33abef511159
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-12-11 13:03:00 -0300

    Update

--- themes/ragnazen/main/navbar.php
+++ themes/ragnazen/main/navbar.php
@@ -3,7 +3,7 @@
 <div class="online-bar">
 	<div class="container">
 		<div class="row online-row d-flex justify-content-end">
-			<ul class="d-flex">
+			<ul class="list-unstyled d-flex">
 				<li class="me-3">Usuários: <span class="green-box">522</span></li>
 				<li>Server: <i class="fa-regular fa-toggle-on"></i></li>
 			</ul>
