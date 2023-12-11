73d9851b271a9a62c1f3a49962c34572c5fea98d
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-12-11 13:06:29 -0300

    Update

--- themes/ragnazen/header.php
+++ themes/ragnazen/header.php
@@ -20,7 +20,7 @@
 		<?php endif ?>
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 		<link rel="stylesheet" href="<?php echo $this->themePath('css/sticky-footer-navbar.css') ?>">
-		<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous">
 	</head>
 	<body>
 
