add60b9fa9c4f13ae0373bc489aa11ee56578321
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-12-11 15:00:24 -0300

    Update

--- themes/ragnazen/main/header.php
+++ themes/ragnazen/main/header.php
@@ -2,12 +2,12 @@
 
 <div class="container-fluid header-main">
     <div class="container">
-    <div class="row">
-        <div class="col-md-6">
+    <div class="row header-line">
+        <div class="col-md-6 header-text">
             <h2 class="nunito">Venha se Divertir!</h2>
             <p>Esteja entre os melhores e obtenha o poder definitivo</p>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-6 header-img">
             <img src="img/img02.png" alt="Image Header ragnarok">
         </div>
     </div>
