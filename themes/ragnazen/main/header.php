a174ec523214ddcee50bf955be524dcdc2e636ae
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-12-11 15:08:40 -0300

    Update

--- themes/ragnazen/main/header.php
+++ themes/ragnazen/main/header.php
@@ -5,7 +5,7 @@
     <div class="row header-line">
         <div class="col-md-6 header-text">
             <h2 class="nunito">Venha se Divertir com os melhores!</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur malesuada tortor imperdiet tellus porttitor placerat. Morbi tincidunt augue et interdum lobortis.</p>
+            <p class="text-header">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur malesuada tortor imperdiet tellus porttitor placerat. Morbi tincidunt augue et interdum lobortis.</p>
         </div>
         <div class="col-md-6 header-img">
             <img src="img/img02.png" alt="Image Header ragnarok">
