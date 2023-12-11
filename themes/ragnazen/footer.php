f8c179a6817fc83a20f742a23aeb07a25870ef86
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-12-11 15:29:04 -0300

    Update

--- themes/ragnazen/footer.php
+++ themes/ragnazen/footer.php
@@ -104,7 +104,7 @@
 	</script>
 <?php endif ?>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script type="text/javascript">
 	$(document).ready(function () {
 		var inputs = 'input[type=text],input[type=password],input[type=file]';
