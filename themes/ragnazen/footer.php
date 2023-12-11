7d7d1276111ef7670d0b4fe25af1d3cbf3ebfaf3
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-12-11 16:04:47 -0300

    Removendo Particulas

--- themes/ragnazen/footer.php
+++ themes/ragnazen/footer.php
@@ -161,12 +161,6 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
 	integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
 	crossorigin="anonymous"></script>
-	<script>
-	<?php
-  $json = file_get_contents($this->themePath('particles.json'));
-  echo $json;
-	?>
-	</script>
 </body>
 
 </html>
