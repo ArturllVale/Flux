43f75743b25db98a2ad843400d906c9a87ac91f9
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-12-11 15:16:48 -0300

    Update

--- themes/ragnazen/main/header.php
+++ themes/ragnazen/main/header.php
@@ -6,7 +6,7 @@
         <div class="col-md-6 header-text">
             <h2 class="nunito">Venha se Divertir com os melhores!</h2>
             <p class="text-header">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur malesuada tortor imperdiet tellus porttitor placerat. Morbi tincidunt augue et interdum lobortis.</p>
-            <button type="button" class="btn btn-primary">Criar Conta</button>
+            <button type="button" class="btn btn-primary"><i class="fas fa-user-circle"></i>Criar Conta</button>
         </div>
         <div class="col-md-6 header-img">
             <img src="img/img02.png" alt="Image Header ragnarok">
