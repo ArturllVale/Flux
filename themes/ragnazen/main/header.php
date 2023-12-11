6ed4c72fae05b24d29a2a2ef1027a05c1d4f7ab6
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-12-11 17:02:33 -0300

    Update

--- themes/ragnazen/main/header.php
+++ themes/ragnazen/main/header.php
@@ -1,12 +1,12 @@
 <?php if (!defined('FLUX_ROOT')) exit; ?>
 
-<div class="container-fluid header-main" id="particles-js">
+<div class="container-fluid header-main">
     <div class="container">
     <div class="row header-line">
         <div class="col-md-6 header-text">
             <h2 class="nunito">Venha se Divertir com os melhores!</h2>
             <p class="text-header">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur malesuada tortor imperdiet tellus porttitor placerat. Morbi tincidunt augue et interdum lobortis.</p>
-            <button type="button" class="btn btn-primary"><p><i class="fas fa-user-circle"></i>Criar Conta</p></button>
+            <button type="button" class="btn btn-primary"><p><i class="fas fa-user-circle"></p></i>Criar Conta</button>
         </div>
         <div class="col-md-6">
         <img class="header-img" src="<?php echo $this->themePath('img/img02.png'); ?>" alt="Image Header ragnarok" />
