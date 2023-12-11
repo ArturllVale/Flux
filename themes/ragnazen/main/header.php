54f5dd421d249780fdfecf4625890b201966c8f6
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-12-11 15:03:40 -0300

    Update Flux

--- themes/ragnazen/main/header.php
+++ themes/ragnazen/main/header.php
@@ -4,8 +4,8 @@
     <div class="container">
     <div class="row header-line">
         <div class="col-md-6 header-text">
-            <h2 class="nunito">Venha se Divertir!</h2>
-            <p>Esteja entre os melhores e obtenha o poder definitivo</p>
+            <h2 class="nunito">Venha se Divertir com os melhores!</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur malesuada tortor imperdiet tellus porttitor placerat. Morbi tincidunt augue et interdum lobortis.</p>
         </div>
         <div class="col-md-6 header-img">
             <img src="img/img02.png" alt="Image Header ragnarok">
