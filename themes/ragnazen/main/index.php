66b565159ac7ea764b4e7d865376d004046ce16b
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-12-13 11:18:50 -0300

    Update css

--- themes/ragnazen/main/index.php
+++ themes/ragnazen/main/index.php
@@ -133,7 +133,7 @@
 						<a class="btn">Nome da Instância 04</a>
 						<small class="sub">Instância</small>
 					</li>
-					<li class="list-group-item instanciado btn" data-bs-toggle="collapse" data-bs-target="#instance01" aria-expanded="false" aria-controls="instance01">
+					<li class="list-group-item more-inst btn" data-bs-toggle="collapse" data-bs-target="#instance01" aria-expanded="false" aria-controls="instance01">
 						<a class="btn">Mais <i class="fab fa-wikipedia-w"></i></a>
 					</li>
 				</ul>
