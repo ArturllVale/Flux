9e373e4e941aeb8906452ca4a364112e18f3b6bc
Artur Vale <47109035+ArturllVale@users.noreply.github.com>
2023-12-13 10:28:35 -0300

    Update Overlay

--- themes/ragnazen/main/index.php
+++ themes/ragnazen/main/index.php
@@ -147,7 +147,9 @@
 				
 					<div class="col-md-4 collapse" id="instance01">
 					<div class="box-instance">
+						<div class="img-overlay">
 						<img class="box-img" src="<?php echo $this->themePath('img/god.png'); ?>" alt="" />
+						</div>
 						<p>Descrição aquo</p>
 						<small>Instância: <span style="color: green;">Disponível</span></small>
 						<hr>
